0569a03c119ec1d175a97129164c51762f731a00
Jaeden Waller <146506880+Fighterplane2@users.noreply.github.com>
2024-07-05 17:13:19 +0000

    dvd finalized

--- main-copy.js
+++ main-copy.js
@@ -9,7 +9,7 @@ let imgHeight = 75;
 let canvasHeight = 700;
 
 function preload(){
-  img = loadImage("./images/dvd.png");
+  img = loadImage('./images/dvd.png');
 }
 
 function setup(){
@@ -20,7 +20,7 @@ function setup(){
 
 function draw(){
   background(255, 100, 60);
-  image(img, xPos, yPos, 100, 75)
+  image(img, xPos, yPos, 100, 75);
   xPos += xSpeed;
   if (xPos > 500-100|| xPos < 0){
     xSpeed = xSpeed * -1;
